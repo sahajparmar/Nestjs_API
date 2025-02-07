6ba5bad5a7aa24e150a998eda51550e35804de83
sahaj parmar <sahajrajput369@gmail.com>
2025-02-07 11:11:08 +0530

    initial commit

---
+++ src/guards/auth.guard.ts
@@ -0,0 +1,12 @@
+import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
+
+@Injectable()
+export class AuthGuard implements CanActivate {
+  canActivate(context: ExecutionContext): boolean {
+    const request = context.switchToHttp().getRequest();
+
+    return request.session.userId;
+  }
+}
+
+export default AuthGuard;
